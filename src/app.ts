bb4a9be3d4f1b53c78ab507838cfd57c00fd2258
이원제 <50200484+leewonje418@users.noreply.github.com>
2020-10-19 17:53:50 +0900

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
 import express from 'express';
+import cors from 'cors';
+import api from './api'
+import bodyParser from 'body-parser';
 
 const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/api", api);
+
+app.listen(5000, () => console.log('Server running'));
