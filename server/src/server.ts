566cf93b2bd5423f0a576cfb979fd90d5ac62e7c
이원제 <50200484+leewonje418@users.noreply.github.com>
2020-11-04 09:21:15 +0900

    [UPDATE] logger사용, db와 연결

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,16 @@
 import * as http from 'http';
 import app from './app';
-// import * as database from './database';
+import * as database from './orm';
 import * as path from 'path'
 import * as dotenv from 'dotenv'
+import logger from './lib/logger';
 
 dotenv.config({ path: path.join(__dirname, '../.env') })
 
 const { PORT } = process.env;
 
-// database.getConnection();
+database.getConnection();
 
 http.createServer(app).listen(PORT || 8080, () => {
-  console.log(`Server is listening to ${PORT}`);
+  logger.green(`Server is listening to ${PORT}`);
 });
