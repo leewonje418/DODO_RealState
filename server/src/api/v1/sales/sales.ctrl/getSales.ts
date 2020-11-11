898145098e3d97ff0e3824641ae3a789f6a411e5
이원제 <50200484+leewonje418@users.noreply.github.com>
2020-11-11 10:34:02 +0900

    Update getSales.ts

--- server/src/api/v1/sales/sales.ctrl/getSales.ts
+++ server/src/api/v1/sales/sales.ctrl/getSales.ts
@@ -1,5 +1,17 @@
 import { Request, Response } from 'express';
-
+import { getRepository } from 'typeorm';
+import Sales from '../../../../entity/Sales'
 export default async (req: Request, res: Response) => {
-
+    try {
+        const salesRepo = getRepository(Sales);
+        const sales: Sales[] = await salesRepo.find({
+            order: {
+                idx: 'DESC'
+            }
+        });
+        console.log(sales);
+        res.send(sales);
+    } catch (error) {
+        
+    }
 }
