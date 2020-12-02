4bccef56daf4ed35e0954fdd9b0e0df672fa453e
이원제 <50200484+leewonje418@users.noreply.github.com>
2020-12-02 10:09:34 +0900

    [UPDATE] 라우터 추가

--- server/src/api/v1/sales/index.ts
+++ server/src/api/v1/sales/index.ts
@@ -1,8 +1,15 @@
+import express from 'express';
 import { Router } from 'express';
 import getSales from './sales.ctrl/getSales'
+import postSales from './sales.ctrl/postSales'
+import multer from 'multer'
+
+const upload = multer({dest: './upload'})
 
 const router = Router();
 
 router.get('/getsales', getSales);
+router.use('/image', express.static('./upload'));
+router.post('/getsales', upload.single('image'), postSales);
 
 export default router;
