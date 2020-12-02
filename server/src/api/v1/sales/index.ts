fb491ede565bfd0df51931d918b00a152cb35fdd
이원제 <50200484+leewonje418@users.noreply.github.com>
2020-12-02 17:34:20 +0900

    [UPDATE] delete 라우터

--- server/src/api/v1/sales/index.ts
+++ server/src/api/v1/sales/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { Router } from 'express';
 import getSales from './sales.ctrl/getSales'
 import postSales from './sales.ctrl/postSales'
+import deleteSales from './sales.ctrl/deleteSales'
 import multer from 'multer'
 
 const upload = multer({dest: './upload'})
@@ -11,5 +12,6 @@ const router = Router();
 router.get('/getsales', getSales);
 router.use('/image', express.static('./upload'));
 router.post('/getsales', upload.single('image'), postSales);
+router.delete('/getsales/:id', deleteSales);
 
 export default router;
