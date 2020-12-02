1758dcbda4a42ad641b062012562ac496c064188
이원제 <50200484+leewonje418@users.noreply.github.com>
2020-12-02 17:34:37 +0900

    [UPDATE] delete 서버 재작

---
+++ server/src/api/v1/sales/sales.ctrl/deleteSales.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import Sales from '../../../../entity/Sales'
+import logger from '../../../../lib/logger';
+export default async (req: Request, res: Response) => {
+    try {
+        const salesRepo = getRepository(Sales);
+        const id = req.params.id;
+        const sales : Sales = await salesRepo.findOne({
+            where : {
+                id
+            }
+        })
+        sales.isDeleted = true;
+        console.log(sales);
+        await salesRepo.save(sales);
+        res.send(sales);
+    } catch (error) {
+        logger.red('server error!', error);
+    }
+}
