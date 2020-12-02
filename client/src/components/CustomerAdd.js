e3af6a4f5b10efdfb08d5650250e2a586e20d624
이원제 <50200484+leewonje418@users.noreply.github.com>
2020-12-02 10:08:55 +0900

    [UPDATE] CustommerAdd

--- client/src/components/CustomerAdd.js
+++ client/src/components/CustomerAdd.js
@@ -9,6 +9,7 @@ class CustomerAdd extends React.Component {
             name: '',
             transaction_type: '',
             market_price: '',
+            area: '',
             address: '',
             like: '',
         }
@@ -20,6 +21,16 @@ class CustomerAdd extends React.Component {
             .then((response) => {
                 console.log(response.data);
             })
+        this.setState({
+            file: null,
+            name: '',
+            transaction_type: '',
+            market_price: '',
+            area: '',
+            address: '',
+            like: ''
+        })
+        window.location.reload();
     }
  
     handleFileChange = (e) => {
@@ -36,13 +47,14 @@ class CustomerAdd extends React.Component {
     }
 
     addCustomer = () => {
-        const url = '/api/customers'
+        const url = 'api/v1/sales/getsales'
         const formData = new FormData();
-        const { file, name, transaction_type, market_price, address, like } = this.state;
+        const { file, name, transaction_type, market_price, area, address, like } = this.state;
         formData.append('image', file);
         formData.append('name', name);
         formData.append('transaction_type', transaction_type);
         formData.append('market_price', market_price);
+        formData.append('area', area);
         formData.append('address', address);
         formData.append('like', like);
         const config = {
@@ -54,16 +66,17 @@ class CustomerAdd extends React.Component {
     }   
 
     render() {
-        const { file, name, transaction_type, market_price, address, like, fileName } = this.state;
+        const { file, name, transaction_type, market_price, area, address, like, fileName } = this.state;
         return (
             <form onSubmit={this.handleFormSubmit}>
                 <h1>고객 추가</h1>
                 프로필 이미지: <input type="file" name="file" file={file} value={fileName} onChange={this.handleFileChange}></input><br/>
                 이름: <input type="text" name="name" value={name} onChange={this.handleValueChange}></input><br/>
-                거래형태: <input type="text" name="transaction_type" value={transaction_type}></input><br/>
-                시세: <input type="text" name="market_price" value={market_price}></input><br/>
-                주소: <input type="text" name="address" value={address}></input><br/>
-                좋아요: <input type="text" name="like" value={like}></input><br/>
+                거래형태: <input type="text" name="transaction_type" value={transaction_type} onChange={this.handleValueChange}></input><br/>
+                시세: <input type="text" name="market_price" value={market_price} onChange={this.handleValueChange}></input><br/>
+                면적: <input type="text" name="area" value={area} onChange={this.handleValueChange}></input><br/>
+                주소: <input type="text" name="address" value={address} onChange={this.handleValueChange}></input><br/>
+                좋아요: <input type="text" name="like" value={like} onChange={this.handleValueChange}></input><br/>
                 <button type="submit">추가하기</button>
             </form>
         )
